a9fc2901399d32ccbe39be2dee3351a49665c7df
Sampriti Panda <sampdingo@hotmail.com>
2016-03-29 23:38:46 +0530

    SWORDS Update (New Soln)

--- IOITC/2014/SWORDS.cpp
+++ IOITC/2014/SWORDS.cpp
@@ -1,59 +1,79 @@
-// Swords of Gilgamesh
-
 #include <iostream>
 #include <algorithm>
-#include <climits>
+#include <deque>
 
 using namespace std;
 
-// dp[i][j] = max(dp[i + 1][j], dp[i + T[i]][D[i]], 1 + dp[i + 1][j - 1])
-// dp[N][j] = 0
-
-int N;
 int T[100000], D[100000];
-int dp[100005];
-int segment_tree[400020];
+// dp[i] = answer for i..N
+int dp[100001];
+int segtree[400004];
+int lazy[400004];
 
-void update(int L, int R, int i, int pos, int val){
-  if(pos < L || pos > R) return;
+void propagate(int L, int R, int i) {
+  if(lazy[i] != 0) {
+    segtree[i] += lazy[i];
 
-  segment_tree[i] = max(segment_tree[i], val);
+    if(L != R) {
+      lazy[2*i + 1] += lazy[i];
+      lazy[2*i + 2] += lazy[i];
+    }
 
-  if(L != R){
-    int mid = L + (R - L)/2;
-    update(L, mid, 2*i + 1, pos, val);
-    update(mid + 1, R, 2*i + 2, pos, val);
+    lazy[i] = 0;
   }
 }
 
-int query(int seg_start, int seg_end, int L, int R, int i){
-  if(seg_start > R || seg_end < L) return 0;
+int query(int L, int R, int i, int qL, int qR) {
+  propagate(L, R, i);
 
-  if(L <= seg_start && seg_end <= R) return segment_tree[i];
+  if(L > R) return 0;
+  if(R < qL || L > qR) return 0;
 
-  int mid = seg_start + (seg_end - seg_start)/2;
-  int left = query(seg_start, mid, L, R, 2*i + 1);
-  int right = query(mid + 1, seg_end, L, R, 2*i + 2);
-  return max(left, right);
+  if(qL <= L && R <= qR) return segtree[i];
+
+  int mid = (L + R)/2;
+
+  return max(query(L, mid, 2*i + 1, qL, qR), query(mid + 1, R, 2*i + 2, qL, qR));
+}
+
+void update(int L, int R, int i, int qL, int qR, int v) {
+  propagate(L, R, i);
+
+  if(L > R) return;
+  if(R < qL || L > qR) return;
+
+  if(qL <= L && R <= qR) {
+    lazy[i] += v;
+    propagate(L, R, i);
+    return;
+  }
+
+  int mid = (L + R)/2;
+  update(L, mid, 2*i + 1, qL, qR, v);
+  update(mid + 1, R, 2*i + 2, qL, qR, v);
+
+  segtree[i] = max(segtree[2*i + 1], segtree[2*i + 2]);
 }
 
 int main() {
-  for(int i = 0; i < 400020; i++) segment_tree[i] = 0;
+  ios::sync_with_stdio(false); cin.tie(0);
 
-  cin >> N;
-  for(int i = 0; i < N; i++){
+  int N; cin >> N;
+  for(int i = 0; i < N; i++) {
     cin >> T[i] >> D[i];
   }
 
-  dp[N - 1] = 0;
   dp[N] = 0;
-  update(0, N + 2, 0, N - 1, N - 2);
-  update(0, N + 2, 0, N, N - 1);
-  for(int i = N - 2; i >= 0; i--){
+  for(int i = N - 1; i >= 0; i--) {
+    // Dodge
     dp[i] = dp[i + 1];
-    dp[i] = max(dp[i], query(0, N + 2, i + T[i] + 2, min(i + T[i] + D[i] + 1, N), 0) - (i + T[i]));
-    update(0, N + 2, 0, i, dp[i] + i - 1);
-  }
+    // Pick and Hit
+    int L = i + T[i] + 1;
+    int R = min(i + T[i] + D[i], N - 1);
+    dp[i] = max(dp[i], query(0, N - 1, 0, L, R) - (L - i - 1));
 
+    update(0, N - 1, 0, i, i, dp[i + 1]);
+    update(0, N - 1, 0, i, N - 1, 1);
+  }
   cout << dp[0] << endl;
 }
